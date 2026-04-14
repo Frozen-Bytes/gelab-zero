e86b65b5e257094f2822b02aa47a659fc52fb7b9
Moe <am6691917@gmail.com>
2026-04-14 12:43:22 +0200

    fix: edited a string format in pu_frontend_executor.py

--- copilot_front_end/pu_frontend_executor.py
+++ copilot_front_end/pu_frontend_executor.py
@@ -197,7 +197,7 @@ def _detect_screen_orientation(device_id):
         adb_command = f"adb -s {device_id}"
     if os.name == 'nt':
         # Windows
-        command = f'{adb_command}' + ''' shell dumpsys input | Select-String 'orientation=\d+' | Select -First 1 | % { $_.Matches.Value -replace 'orientation=', '' }'''
+        command = f'{adb_command}' + r""" shell dumpsys input | Select-String 'orientation=\d+' | Select -First 1 | % { $_.Matches.Value -replace 'orientation=', '' }"""
         
         # 使用 subprocess 运行 PowerShell 命令
         result = subprocess.run(
